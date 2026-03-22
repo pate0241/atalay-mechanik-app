f6505ab98f6747ffbc2e12dc92d9d63ae5e8578f
Fatih Atalay <mekanikatalay@gmail.com>
2026-03-22 14:59:59 +0300

    fix: use webpack build and remove extra lockfile issue

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    turbo: false, // 🔥 GANZ WICHTIG
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
